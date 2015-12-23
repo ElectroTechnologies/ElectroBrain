c46cfe4d23f60e432e5ac57eafadf8ab5675ebe5
Amrit Shenava <amritshenava98@users.noreply.github.com>
2015-12-22 22:46:53 -0800

    Update Mainforpersonaluse.py

--- Mainforpersonaluse.py
+++ Mainforpersonaluse.py
@@ -1,2 +1,12 @@
+'''This is a python source code for making AI(Articial Intelligence) programs for any device including The Raspberry Pi, Arduino and 
+other devices. This ElectroBrain is recommended to be used for personal use. This code will be modified weekly and is currently in the 
+basic stage of Development 
+'''
 def main():
-    
+    name=raw_input("What is your name?")
+    age=int(input("How old are you?")
+    gender=raw_input("What is your gender?")
+    nationality=raw_input("What is your nationality?")
+    origin=raw_input("Where are you originally from?")
+    return [name,age,gender,nationality,origin]
+main()
